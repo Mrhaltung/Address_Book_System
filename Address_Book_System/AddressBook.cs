8e85c82684d58b8bd6d1ec4a6fe71a5cff2e697c
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-26 11:46:34 +0530

    Edit Existing contact person using their name

--- Address_Book_System/AddressBook.cs
+++ Address_Book_System/AddressBook.cs
@@ -10,28 +10,28 @@ namespace Address_Book_System
 
         public static void AddContact()
         {
-            Console.Write("How many contacts do you want to add? : ");
-            int contactsNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" You have to enter your details here. \n");
+            int contactsNum = 1;
             while (contactsNum > 0)
             {
                 PersonsDetails person = new PersonsDetails();
 
 
-                Console.Write("Enter your First name : ");
+                Console.Write(" Enter your First name : ");
                 person.Firstname = Console.ReadLine();
-                Console.Write("Enter your Last name : ");
+                Console.Write(" Enter your Last name : ");
                 person.LastName = Console.ReadLine();
-                Console.Write("Enter your Address : ");
+                Console.Write(" Enter your Address : ");
                 person.Address = Console.ReadLine();
-                Console.Write("Enter your City : ");
+                Console.Write(" Enter your City : ");
                 person.City = Console.ReadLine();
-                Console.Write("Enter your State : ");
+                Console.Write(" Enter your State : ");
                 person.State = Console.ReadLine();
-                Console.Write("Enter your Zip code : ");
+                Console.Write(" Enter your Zip Code : ");
                 person.ZipCode = Console.ReadLine();
-                Console.Write("Enter your Phone number : ");
+                Console.Write(" Enter your Phone Number : ");
                 person.PhoneNumber = Console.ReadLine();
-                Console.Write("Enter your Email ID : ");
+                Console.Write(" Enter your Email-ID : ");
                 person.Email = Console.ReadLine();
 
                 addressBook.Add(person);
@@ -59,15 +59,84 @@ namespace Address_Book_System
                         Console.WriteLine(" E-Mail ID-->{0}", contact.Email);
                         break;
                     }
-                    else
-                    {
-                        Console.WriteLine("contact of the person {0} does not exist", nameKey);
-                    }
                 }
+                //Console.WriteLine(" Contact of the person {0} does not exist", nameKey);
             }
             else
             {
-                Console.WriteLine("Your address book is empty");
+                Console.WriteLine(" Your address book is empty");
+            }
+        }
+
+        public static void Edit()
+        {
+            Console.Write("\n Enter the first name of the person whom you want to edit the details : ");
+            string editKey = Console.ReadLine();
+            if(addressBook.Count > 0)
+            {
+                foreach(PersonsDetails persons in addressBook)
+                {
+                    if(editKey.ToLower() == persons.Firstname.ToLower())
+                    {
+                        Console.WriteLine("\n Enter the key number for editing the details." +
+                            "\n1.First Name " +
+                            "\n2.Last Name " +
+                            "\n3.Address " +
+                            "\n4.City " +
+                            "\n5.State " +
+                            "\n6.Zipcode " +
+                            "\n7.Phone Number " +
+                            "\n8.Email ID");
+                        Console.Write(" Enter your option : ");
+                        int key = Convert.ToInt32(Console.ReadLine());
+                        switch (key)
+                        {
+                            case 1:
+                                Console.Write("Enter the new First Name : ");
+                                persons.Firstname = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the new Last name");
+                                persons.LastName = Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.Write(" Enter the New Address : ");
+                                persons.Address = Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.Write(" Enter the New City : ");
+                                persons.City = Console.ReadLine();
+                                break;
+                            case 5:
+                                Console.Write(" Enter the New State : ");
+                                persons.State = Console.ReadLine();
+                                break;
+                            case 6:
+                                Console.Write("Enter the New Zip Code : ");
+                                persons.ZipCode = Console.ReadLine();
+                                break;
+                            case 7:
+                                Console.Write(" Enter the New Phone Number : ");
+                                persons.PhoneNumber = Console.ReadLine();
+                                break;
+                            case 8:
+                                Console.Write(" Enter the New EMail-ID : ");
+                                persons.Email = Console.ReadLine();
+                                break;
+                            default:
+                                Console.WriteLine(" Please enter a valid input");
+                                Edit();
+                                break;
+                        }
+                        Console.WriteLine(" {0}'s contact has been successfully added", editKey);
+                        break;
+                    }                   
+                }
+                //Console.WriteLine(" Contact of the person {0} does not exist", editKey);
+            }
+            else
+            {
+                Console.WriteLine(" Your address book is empty");
             }
         }
     }
